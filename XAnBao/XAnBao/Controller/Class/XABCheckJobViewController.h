ddcc1b94fee8bfaf0a3589f775bba05179027751
磊 <minglei1@staff.sina.com.cn>
2017-03-29 15:06:53 +0800

    模块动态切换家长老师角色切换

---
+++ XAnBao/XAnBao/Controller/Class/XABCheckJobViewController.h
@@ -0,0 +1,13 @@
+//
+//  XABCheckJobViewController.h
+//  XAnBao
+//
+//  Created by Minlay on 17/3/29.
+//  Copyright © 2017年 Minlay. All rights reserved.
+//
+
+#import "YBBaseViewController.h"
+
+@interface XABCheckJobViewController : YBBaseViewController
+
+@end
